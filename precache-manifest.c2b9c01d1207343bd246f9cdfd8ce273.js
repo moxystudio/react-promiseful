67c91e17112a9f4707ea20cb42f95b554af3ee08
André Cruz <andremiguelcruz@msn.com>
2019-05-05 23:19:28 +0100

    Updates

---
+++ precache-manifest.c2b9c01d1207343bd246f9cdfd8ce273.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3838cc805f280dbc0449f95ab4c56574",
+    "url": "/react-promiseful/index.html"
+  },
+  {
+    "revision": "9195f705063c0dd4cb3b",
+    "url": "/react-promiseful/static/css/main.dede6312.chunk.css"
+  },
+  {
+    "revision": "80a46f23a296339847bc",
+    "url": "/react-promiseful/static/js/2.1ac58020.chunk.js"
+  },
+  {
+    "revision": "9195f705063c0dd4cb3b",
+    "url": "/react-promiseful/static/js/main.fe8412eb.chunk.js"
+  },
+  {
+    "revision": "54ee8c1e078f22cd24a9",
+    "url": "/react-promiseful/static/js/runtime~main.a744388c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd41970e768dd7f18325bbe2611c3f80.js"
+  "/react-promiseful/precache-manifest.c2b9c01d1207343bd246f9cdfd8ce273.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-promiseful/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
