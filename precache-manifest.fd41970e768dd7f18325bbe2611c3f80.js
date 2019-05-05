66cb7c49cee5edf3e5b81b19db40d8672da23767
André Cruz <andremiguelcruz@msn.com>
2019-05-05 23:16:40 +0100

    Updates

---
+++ precache-manifest.fd41970e768dd7f18325bbe2611c3f80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818a131d914f99248b88d8832d041b1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9195f705063c0dd4cb3b",
+    "url": "/static/css/main.dede6312.chunk.css"
+  },
+  {
+    "revision": "80a46f23a296339847bc",
+    "url": "/static/js/2.1ac58020.chunk.js"
+  },
+  {
+    "revision": "9195f705063c0dd4cb3b",
+    "url": "/static/js/main.fe8412eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
